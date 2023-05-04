d72434c039e62334fc4c251de9d188adc9117727
CanadianDoc <78990604+CanadianDoc@users.noreply.github.com>
2023-05-04 13:14:36 -0600

    menu setup

--- scripts/ease.js
+++ scripts/ease.js
@@ -1,32 +1,61 @@
 function scrollToDestination(destination) {
-  const navbar = document.querySelector(".navbar");
-  const navbarHeight =
-    navbar.getBoundingClientRect().height;
-  const destinationTop =
-    destination.offsetTop - navbarHeight; // get the top position of the destination section
-  const scrollOptions = {
-    top: destinationTop, // scroll to the top of the destination section
-    behavior: "smooth", // enable smooth scrolling
-    duration: 1000,
-  };
+	const navbar = document.querySelector(".navbar");
+	const navbarHeight =
+		navbar.getBoundingClientRect().height;
+	const destinationTop =
+		destination.offsetTop - navbarHeight; // get the top position of the destination section
+	const scrollOptions = {
+		top: destinationTop, // scroll to the top of the destination section
+		behavior: "smooth", // enable smooth scrolling
+		duration: 1000,
+	};
 
-  window.scrollTo(scrollOptions);
+	window.scrollTo(scrollOptions);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const logoLink = document.querySelector(".navLogo");
-  const aboutLink = document.querySelector(".navLink");
-  const introDestination = document.querySelector(".intro");
-  const aboutDestination =
-    document.querySelector(".aboutMe");
+	const logoLink = document.querySelector(".navLogo");
+	const introDestination = document.querySelector(".intro");
 
-  logoLink.addEventListener("click", (event) => {
-    event.preventDefault(); // prevent the default link behavior
-    scrollToDestination(introDestination);
-  });
+	const skillLink = document.querySelector("#skills");
+	const skillDestination =
+		document.querySelector(".skills");
 
-  aboutLink.addEventListener("click", (event) => {
-    event.preventDefault(); // prevent the default link behavior
-    scrollToDestination(aboutDestination);
-  });
+	const projectLink = document.querySelector("#projects");
+	const projectDestination =
+		document.querySelector(".projects");
+
+	const aboutMeLink = document.querySelector("#aboutMe");
+	const aboutMeDestination =
+		document.querySelector(".aboutMe");
+
+	const contactMeLink =
+		document.querySelector("#contactMe");
+	const contactMeDestination =
+		document.querySelector(".contactMe");
+
+	logoLink.addEventListener("click", (event) => {
+		event.preventDefault(); // prevent the default link behavior
+		scrollToDestination(introDestination);
+	});
+
+	skillLink.addEventListener("click", (event) => {
+		event.preventDefault(); // prevent the default link behavior
+		scrollToDestination(skillDestination);
+	});
+
+	projectLink.addEventListener("click", (event) => {
+		event.preventDefault(); // prevent the default link behavior
+		scrollToDestination(projectDestination);
+	});
+
+	aboutMeLink.addEventListener("click", (event) => {
+		event.preventDefault(); // prevent the default link behavior
+		scrollToDestination(aboutMeDestination);
+	});
+
+	contactMeLink.addEventListener("click", (event) => {
+		event.preventDefault(); // prevent the default link behavior
+		scrollToDestination(contactMeDestination);
+	});
 });
--- scripts/menuScript.js
+++ scripts/menuScript.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const menuIcon = document.querySelector(".menuIcon");
 	const navMenu = document.querySelector(".navMenu");
+	const navLogo = document.querySelector(".navLogo");
 
 	menuIcon.addEventListener("click", () => {
 		menuIcon.classList.toggle("active");
@@ -13,4 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
 			navMenu.classList.remove("active");
 		})
 	);
+	navLogo.addEventListener("click", () => {
+		menuIcon.classList.remove("active");
+		navMenu.classList.remove("active");
+	});
 });
