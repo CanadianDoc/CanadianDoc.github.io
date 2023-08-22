b2be70376f2cb339a3dd2be78c932f154a64af39
CanadianDoc <78990604+CanadianDoc@users.noreply.github.com>
2023-08-22 04:32:31 -0600

    lightMode/darkmode

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,21 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const darkMode = document.querySelector("#darkMode");
-	const body = document.querySelector("body");
-	const darkModeText =
-		document.querySelector("#darkModeText");
+  const darkMode = document.querySelector("#darkMode");
+  const body = document.querySelector("body");
 
-	darkMode.addEventListener("click", function () {
-		body.classList.toggle("darkMode");
-		if (body.classList.contains("darkMode")) {
-			darkMode
-				.querySelector("i")
-				.classList.replace("fa-regular", "fa-solid");
-			darkModeText.textContent = "DarkMode";
-		} else {
-			darkMode
-				.querySelector("i")
-				.classList.replace("fa-solid", "fa-regular");
-			darkModeText.textContent = "LightMode";
-		}
-	});
+  darkMode.addEventListener("click", function () {
+    body.classList.toggle("darkMode");
+
+    if (body.classList.contains("darkMode")) {
+      darkMode.querySelector("i").classList.replace("fa-moon", "fa-sun");
+      darkMode.setAttribute("data", "Dark Mode");
+    } else {
+      darkMode.querySelector("i").classList.replace("fa-sun", "fa-moon");
+      darkMode.setAttribute("data", "Light Mode");
+    }
+  });
 });
